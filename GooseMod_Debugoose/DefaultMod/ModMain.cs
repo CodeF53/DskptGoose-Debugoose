b8dac367f1ac3d009c61436dd09b1ce2c45d1c24
CodeF53 <37855219+CodeF53@users.noreply.github.com>
2020-02-24 00:03:32 -0700

    Add Debugoose v1 files
    Add all of the VisualStudioFiles Used

---
+++ GooseMod_Debugoose/DefaultMod/ModMain.cs
@@ -0,0 +1,106 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using GooseShared;
+using SamEngine;
+using static GooseShared.API;
+using static GooseShared.API.TaskDatabaseQueryFunctions;
+
+namespace DefaultMod
+{
+    public class ModEntryPoint : IMod
+    {
+        public SolidBrush brushBlack, brushWhite, brushGreen;
+        Font consolas;
+
+        String[] tasks;
+
+        int InfolnNum;
+        int ConslnNum;
+        StringWriter cnslStream;
+
+
+
+        void IMod.Init()
+        {
+            cnslStream = new StringWriter();
+            Console.SetOut(cnslStream);
+
+            tasks = API.TaskDatabase.getAllLoadedTaskIDs();
+            brushBlack = new SolidBrush(Color.FromArgb(255/* /2 */, Color.Black));
+            brushGreen = new SolidBrush(ColorTranslator.FromHtml("#4AF626"));
+
+            InjectionPoints.PostRenderEvent += PostRender;
+        }
+
+        public void PostRender(GooseEntity g, Graphics graph)
+        {
+            InfolnNum = 0;
+            ConslnNum = 0;
+
+            drawTask(g, graph);
+            drawXYpos(g, graph);
+
+            drawConsole(g, graph);
+        }
+
+        public void InfoLine(GooseEntity g, Graphics graph, String txt)
+        {
+            Size txtLen = graph.MeasureString(txt, SystemFonts.DefaultFont).ToSize();
+            PointF txtPos = new PointF(g.position.x + 25, g.position.y + 25 + 14 * InfolnNum);
+
+            graph.FillRectangle(brushBlack, new Rectangle(Point.Round(txtPos), txtLen));
+            graph.DrawString(txt, SystemFonts.DefaultFont, brushGreen, txtPos);
+
+            InfolnNum++;
+        }
+        public void ConsLine(GooseEntity g, Graphics graph, String txt)
+        {
+            Size txtLen = graph.MeasureString(txt, SystemFonts.DefaultFont).ToSize();
+            Size refLen = graph.MeasureString("aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa", SystemFonts.DefaultFont).ToSize();
+            if (txtLen.Width > refLen.Width)
+            {
+                refLen.Width = txtLen.Width;
+            }
+
+            PointF txtPos = new PointF(g.position.x + 25, g.position.y + 25 + 14 * InfolnNum + 13*ConslnNum);
+
+            graph.FillRectangle(brushBlack, new Rectangle(Point.Round(txtPos), refLen));
+            graph.DrawString(txt, SystemFonts.DefaultFont, brushGreen, txtPos);
+
+            ConslnNum++;
+        }
+
+        public void drawConsole(GooseEntity g, Graphics graph)
+        {
+
+            String[] consoleLines = cnslStream.ToString().Split(new[] {'\r','\n'});
+
+            if (!(consoleLines.Length <= 0))
+            {
+                for (int i = consoleLines.Length - 21; i < consoleLines.Length - 1; i += 2)
+                {
+                    if (!(i <= -1))
+                    {
+                        ConsLine(g, graph, consoleLines[i]);
+                    }
+                }
+            }
+        }
+
+        public void drawXYpos(GooseEntity g, Graphics graph)
+        {
+            Vector2 gPos = g.position;
+            String gp = "xy: (" + Math.Round(gPos.x) + ", " + Math.Round(gPos.y) + ")";
+            InfoLine(g, graph, gp);
+        }
+        public void drawTask(GooseEntity g, Graphics graph)
+        {
+            InfoLine(g, graph, tasks[g.currentTask]);
+        }
+    }
+}
